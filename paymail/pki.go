dbedf96a7166b4e5c689379beff185fda76a9eb7
mrz1836 <mrz1818@gmail.com>
2020-04-10 01:19:57 -0400

    More inline validations in the PKI request

--- paymail/pki.go
+++ paymail/pki.go
@@ -19,9 +19,9 @@ Default:
 
 // PKIResponse is the result returned
 type PKIResponse struct {
-	BsvAlias string `json:"bsvalias"`
-	Handle   string `json:"handle"`
-	PubKey   string `json:"pubkey"`
+	BsvAlias string `json:"bsvalias"` // Version
+	Handle   string `json:"handle"`   // The <alias>@<domain>.<tld>
+	PubKey   string `json:"pubkey"`   // The related PubKey
 }
 
 // GetPKI will return a valid PKI response
@@ -34,12 +34,12 @@ func GetPKI(pkiUrl, alias, domain string) (pki *PKIResponse, err error) {
 
 	// Start the request
 	var req *http.Request
-	if req, err = http.NewRequest("GET", reqURL, nil); err != nil {
+	if req, err = http.NewRequest(http.MethodGet, reqURL, nil); err != nil {
 		return
 	}
 
 	// Set the headers (standard user agent so it cannot be blocked)
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36")
+	req.Header.Set("User-Agent", defaultUserAgent)
 
 	// Fire the request
 	var resp *http.Response
@@ -54,13 +54,34 @@ func GetPKI(pkiUrl, alias, domain string) (pki *PKIResponse, err error) {
 
 	// Test the status code
 	// Only 200 and 304 are accepted
-	if resp.StatusCode != 200 && resp.StatusCode != 304 {
+	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusNotModified {
 		err = fmt.Errorf("bad response from paymail provider: %d", resp.StatusCode)
 		return
 	}
 
 	// Try and decode the response
-	err = json.NewDecoder(resp.Body).Decode(&pki)
+	if err = json.NewDecoder(resp.Body).Decode(&pki); err != nil {
+		return
+	}
+
+	// Invalid version?
+	if len(pki.BsvAlias) == 0 {
+		err = fmt.Errorf("missing bsvalias version")
+		return
+	}
+
+	// Check basic requirements (handle)
+	if pki.Handle != alias+"@"+domain {
+		err = fmt.Errorf("pki response handle %s does not match paymail address: %s", pki.Handle, alias+"@"+domain)
+		return
+	}
+
+	// Check the PubKey length
+	if len(pki.PubKey) == 0 {
+		err = fmt.Errorf("pki response is missing a PubKey value")
+	} else if len(pki.PubKey) != pubKeyLength {
+		err = fmt.Errorf("returned pubkey is not the required length of %d, got: %d", pubKeyLength, len(pki.PubKey))
+	}
 
 	return
 }
