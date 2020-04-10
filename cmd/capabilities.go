9e76039badb5cc4745149f71cfb2631dcb0e5477
mrz1836 <mrz1818@gmail.com>
2020-04-10 01:21:13 -0400

    Cleanup and mods for capabilities cmd

--- cmd/capabilities.go
+++ cmd/capabilities.go
@@ -16,6 +16,7 @@ var capabilitiesCmd = &cobra.Command{
 	Short:   "Get the capabilities of the paymail domain",
 	Long:    `This command will return the capabilities for a given paymail domain`,
 	Aliases: []string{"abilities"},
+	Example: "capabilities " + defaultDomainName,
 	Args: func(cmd *cobra.Command, args []string) error {
 		if len(args) < 1 {
 			return fmt.Errorf("%s requires either a domain or paymail address\n", logPrefix)
@@ -39,6 +40,7 @@ var capabilitiesCmd = &cobra.Command{
 		}
 
 		// Get the details from the SRV record
+		fmt.Printf("%s getting SRV record...\n", logPrefix)
 		srv, err := paymail.GetSRVRecord(serviceName, protocol, domain, nameServer)
 		if err != nil {
 			fmt.Printf("%s get SRV record failed: %s\n", logPrefix, err.Error())
@@ -46,22 +48,36 @@ var capabilitiesCmd = &cobra.Command{
 		}
 
 		// Get the capabilities for the given domain
-		var resp *paymail.CapabilitiesResponse
-		resp, err = paymail.GetCapabilities(srv.Target, int(srv.Port))
+		fmt.Printf("%s getting capabilities...\n", logPrefix)
+		var capabilities *paymail.CapabilitiesResponse
+		capabilities, err = paymail.GetCapabilities(srv.Target, int(srv.Port))
 		if err != nil {
 			fmt.Printf("%s get capabilities failed: %s\n", logPrefix, err.Error())
 			return
 		}
 
+		// Check the version
+		if capabilities.BsvAlias != bsvAliasVersion {
+			fmt.Printf("%s capabilities bsvalias version mismatch, expected: %s but got: %s\n", logPrefix, bsvAliasVersion, capabilities.BsvAlias)
+			return
+		}
+
 		// Show basic results
-		fmt.Printf("%s BsvAlias version: %s\n", logPrefix, resp.BsvAlias)
-		fmt.Printf("%s capabilities found: %d\n", logPrefix, len(resp.Capabilities))
+		fmt.Printf("%s bsvalias version: %s\n", logPrefix, capabilities.BsvAlias)
+		fmt.Printf("%s capabilities found: %d\n", logPrefix, len(capabilities.Capabilities))
 
 		// Show all the found capabilities
 		fmt.Printf("%s capabilities activated:\n", logPrefix)
-		for key, val := range resp.Capabilities {
-			if reflect.TypeOf(val).String() == "string" {
+		for key, val := range capabilities.Capabilities {
+			valType := reflect.TypeOf(val).String()
+			if valType == "string" {
 				fmt.Printf("%s capability: %s target: %s\n", logPrefix, key, val)
+			} else if valType == "bool" { // See: http://bsvalias.org/04-02-sender-validation.html
+				if val.(bool) {
+					fmt.Printf("%s capability: %s is enabled\n", logPrefix, key)
+				} else {
+					fmt.Printf("%s capability: %s is disabled\n", logPrefix, key)
+				}
 			}
 		}
 	},
@@ -71,5 +87,5 @@ func init() {
 	rootCmd.AddCommand(capabilitiesCmd)
 
 	// Custom port for the SRV record (target address)
-	capabilitiesCmd.Flags().IntVarP(&port, "port", "p", defaultPort, "Port that is found in the SRV record")
+	capabilitiesCmd.Flags().IntVarP(&port, "port", "p", paymail.DefaultPort, "Port that is found in the SRV record")
 }
