8e32fc466c2df8fb294c364efbb984ac975e1c25
mrz1836 <mrz1818@gmail.com>
2020-04-11 20:08:20 -0400

    Moved into its own function

--- cmd/utilities.go
+++ cmd/utilities.go
@@ -4,7 +4,9 @@ import (
 	"crypto/rand"
 	"encoding/hex"
 	"fmt"
+	"strings"
 
+	"github.com/mrz1836/go-validate"
 	"github.com/mrz1836/paymail-inspector/chalker"
 	"github.com/mrz1836/paymail-inspector/paymail"
 )
@@ -39,5 +41,30 @@ func getPki(url, alias, domain string) (pki *paymail.PKIResponse, err error) {
 		chalker.Log(chalker.WARN, fmt.Sprintf("pubkey length is: %d, expected: %d", len(pki.PubKey), paymail.PubKeyLength))
 	}
 
+	return
+}
+
+// validatePaymailAndDomain will do a basic validation on the paymail format
+func validatePaymailAndDomain(paymailAddress, domain string) (valid bool) {
+
+	// Validate the format for the paymail address (paymail addresses follow conventional email requirements)
+	if ok, err := validate.IsValidEmail(paymailAddress, false); err != nil {
+		chalker.Log(chalker.ERROR, fmt.Sprintf("paymail address failed format validation: %s", err.Error()))
+		return
+	} else if !ok {
+		chalker.Log(chalker.ERROR, "paymail address failed format validation: unknown reason")
+		return
+	}
+
+	// Check for a real domain (require at least one period)
+	if !strings.Contains(domain, ".") {
+		chalker.Log(chalker.ERROR, fmt.Sprintf("domain name is invalid: %s", domain))
+		return
+	} else if !validate.IsValidDNSName(domain) { // Basic DNS check (not a REAL domain name check)
+		chalker.Log(chalker.ERROR, fmt.Sprintf("domain name failed DNS check: %s", domain))
+		return
+	}
+
+	valid = true
 	return
 }
