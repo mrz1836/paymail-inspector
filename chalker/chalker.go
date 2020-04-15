43dbbc0c209269cb7aecc58a6af149307858e5c7
mrz1836 <mrz1818@gmail.com>
2020-04-14 20:20:29 -0400

    Removed log prefix (too verbose)

--- chalker/chalker.go
+++ chalker/chalker.go
@@ -19,37 +19,28 @@ const (
 )
 
 var (
-	logPrefix string
-	spacer    string
+	spacer string
 )
-
-// SetPrefix for the logs in the CLI application output
-func SetPrefix(prefix string) {
-	logPrefix = prefix
-	if len(logPrefix) > 0 {
-		spacer = " "
-	}
-}
 
 // Error chalks and returns an error
 func Error(body string) error {
-	return fmt.Errorf("%s%s %s%s", chalk.Magenta, chalk.Dim.TextStyle(logPrefix), body, chalk.Reset)
+	return fmt.Errorf("%s %s%s", chalk.Magenta, body, chalk.Reset)
 }
 
 // Log chalks stuff to console, returns nothing
 func Log(level string, body string) {
 	switch level {
 	case INFO:
-		fmt.Printf("%s%s%s%s%s\n", chalk.Cyan, chalk.Dim.TextStyle(logPrefix), spacer, body, chalk.Reset)
+		fmt.Printf("%s%s%s%s\n", chalk.Cyan, spacer, body, chalk.Reset)
 	case WARN:
-		fmt.Printf("%s%s%s%s%s\n", chalk.Yellow, chalk.Dim.TextStyle(logPrefix), spacer, body, chalk.Reset)
+		fmt.Printf("%s%s%s%s\n", chalk.Yellow, spacer, body, chalk.Reset)
 	case ERROR:
-		fmt.Printf("%s%s%s%s%s\n", chalk.Magenta, chalk.Dim.TextStyle(logPrefix), spacer, body, chalk.Reset)
+		fmt.Printf("%s%s%s%s\n", chalk.Magenta, spacer, body, chalk.Reset)
 	case SUCCESS:
-		fmt.Printf("%s%s%s%s%s\n", chalk.Green, chalk.Dim.TextStyle(logPrefix), spacer, body, chalk.Reset)
+		fmt.Printf("%s%s%s%s\n", chalk.Green, spacer, body, chalk.Reset)
 	case DEFAULT:
 		fallthrough
 	default:
-		fmt.Printf("%s%s%s\n", chalk.Dim.TextStyle(logPrefix), spacer, body)
+		fmt.Printf("%s%s\n", spacer, body)
 	}
 }
