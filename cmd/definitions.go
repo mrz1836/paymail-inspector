af09cd8164b4547332518ad5a897feaf8bce4aad
mrz1836 <mrz1818@gmail.com>
2020-10-01 19:09:45 -0400

    Bumped version

--- cmd/definitions.go
+++ cmd/definitions.go
@@ -10,7 +10,7 @@ import (
 )
 
 // Version is set manually (also make:build overwrites this value from Github's latest tag)
-var Version = "v0.3.0"
+var Version = "v0.3.1"
 
 // Default flag values for various commands
 var (
