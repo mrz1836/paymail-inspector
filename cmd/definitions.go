c913929e774f0b8d6ae8b6694faf1ce34a3301a8
mrz1836 <mrz1818@gmail.com>
2022-04-11 09:57:02 -0400

    Bumped version

--- cmd/definitions.go
+++ cmd/definitions.go
@@ -10,7 +10,7 @@ import (
 )
 
 // Version is set manually (also make:build overwrites this value from Github's latest tag)
-var Version = "v0.3.21"
+var Version = "v0.3.22"
 
 // Default flag values for various commands
 var (
